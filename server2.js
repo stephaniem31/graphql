3bf02610b01519b7f5585602c44de9cea6227ae0
Stephanie Mariani <stephanie.mariani31@gmail.com>
2021-10-26 14:17:42 +0200

    wip

---
+++ server2.js
@@ -0,0 +1,138 @@
+var express = require('express');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+var { buildSchema } = require('graphql');
+
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+        title(title : String): [Course]
+        author(author : String): [Course]
+    },
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+        newCourse(course : CourseInput): [Course]
+        deleteCourse(course: CourseInput): [Course]
+    },
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
+    },
+    input CourseInput {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
+    },
+`);
+
+var coursesData = [
+    {
+        id: 1,
+        title: 'The Complete Node.js Developer Course',
+        author: 'Andrew Mead, Rob Percival',
+        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs/'
+    },
+    {
+        id: 2,
+        title: 'Node.js, Express & MongoDB Dev to Deployment',
+        author: 'Brad Traversy',
+        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+    },
+    {
+        id: 3,
+        title: 'JavaScript: Understanding The Weird Parts',
+        author: 'Anthony Alicea',
+        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        topic: 'JavaScript',
+        url: 'https://codingthesmartway.com/courses/understand-javascript/'
+    }
+]
+
+var getCourse = function(args) { 
+    var id = args.id;
+    return coursesData.filter(course => {
+        return course.id == id;
+    })[0];
+}
+
+var getCourses = function(args) {
+    if (args.topic) {
+        var topic = args.topic;
+        return coursesData.filter(course => course.topic === topic);
+    } else {
+        return coursesData;
+    }
+}
+
+var getTitle = function(args) {
+    if(args.title) {
+        var title = args.title;
+        return coursesData.filter(course => course.title.includes(title));
+    } else {
+        return coursesData
+    }
+    }
+
+var getAuthor = function(args) {
+    if(args.author) {
+        var author = args.author;
+        return coursesData.filter(course => course.author.includes(author));
+    } else {
+        return coursesData
+    }
+}
+
+var updateCourseTopic = function({id, topic}) {
+    coursesData.map(course => {
+        if (course.id === id) {
+            course.topic = topic;
+            return course;
+        }
+    });
+    return coursesData.filter(course => course.id === id) [0];
+}
+
+var createCourse = function(arg) {
+    coursesData.push(arg.course)
+    return coursesData;
+    };
+
+/*var deleteCourse = function({id}) {
+    const index = 
+    coursesData.splice(0, 1)
+    return coursesData;
+}
+*/
+
+var root = {
+    course: getCourse,
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic,
+    newCourse: createCourse,
+    title: getTitle,
+    author: getAuthor,
+    /*delete: deleteCourse,*/
+
+};
+
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
